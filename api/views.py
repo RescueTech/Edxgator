a5e3c635036fac986e2f9e538420763c9e43fc8c
Sa3d <ahmed.saad@backmarket.com>
2019-02-24 14:42:28 +0100

    Feat: enhances course_serializer

--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from bson import ObjectId
 from starlette.responses import JSONResponse
 from starlette.endpoints import HTTPEndpoint
@@ -6,7 +8,17 @@ from gateway.managers import course_manager
 
 
 def course_serializer(course):
+    """
+    Responsible for serializing a course object, and return it in the format suitable for parsing as JSON,
+    and also responsible for making the presentation layer enhancement, for example, converting a datetime into a date
+    object when it's needed for prettification purposes.
+    This layer isn't responsible for making sure that every field is in its correct type.
+        ex. don't do checks to see if the start_date is really a date.. only that it's there and not null or empty str.
+    """
     course['_id'] = str(course['_id'])
+    course['start'] = (course.get('start') or '').split('T')[0]
+    course['end'] = (course.get('end') or '').split('T')[0]
+    course['pacing_type'] = course.get('pacing_type', None)
     return course
 
 
